37f67763db4aa669a33887ef2929c8a07e6021f9
Alexander Daley <alexander.daley@mymail.champlain.edu>
2025-09-01 12:46:44 -0400

    Class commit for week 2

---
+++ Week2/Lab1.ps1
@@ -0,0 +1,37 @@
+﻿clear
+#Q1
+#Get-NetIPAddress | Where-Object { $_.InterfaceAlias -ilike "Ethernet" } | Select IPAddress
+
+#Q2
+#Get-NetIPAddress | Where-Object { $_.InterfaceAlias -ilike "Ethernet" } | Select-Object PrefixLength
+
+#Q3/Q4
+#Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_*" } | Sort-Object
+
+#Q5/Q6
+#Get-CimInstance Win32_NetworkAdapterConfiguration | Select DHCPServer
+
+#Q7
+#(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "Ethernet"}).ServerAddresses[0]
+
+#Q8
+cd $PSScriptRoot
+$files=(Get-ChildItem -File)
+for($j = 0; $j -le $files.Length; $j++)
+{
+    if($files[$j].Name -ilike "*ps1")
+    {
+        Write-Host $files[$j].Name
+    }
+}
+
+#Q9
+$folderPath = Join-Path $PSScriptRoot "outfolder"
+if(Test-Path -Path $folderPath)
+{
+    Write-Host "Folder already exists"
+}
+else
+{
+    New-Item -Path $folderPath -Name "outfolder" -ItemType Directory
+}
