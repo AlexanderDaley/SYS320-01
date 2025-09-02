bf8c2b9d21cc89164da66d9a958b6acf81a86e3a
Alexander Daley <alexander.daley@mymail.champlain.edu>
2025-09-02 15:26:42 -0400

    Finished Process Management assignment

---
+++ Week2/A1-ProcessManagement.ps1
@@ -0,0 +1,19 @@
+﻿clear
+#P1
+#Get-Process | Where-Object{ $_.ProcessName -ilike "C*" }
+
+#P2
+#Get-Process | Where-Object { $_.ProcessName -inotlike "*system32*" }
+
+#P3
+#Get-Service | Where-Object {$_.Status -eq "Stopped" } | Sort-Object
+
+#P4
+if(Get-Process -Name "A1-ProcessManagement" -ErrorAction SilentlyContinue)
+{
+    Write-Host "Exiting"
+    Exit
+}else{
+    Start-Process chrome.exe -ArgumentList "--new-window https://www.champlain.edu"
+    Write-Host "Tab Opened"
+}
