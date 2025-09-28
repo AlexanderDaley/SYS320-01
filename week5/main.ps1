2ba8ae45004572ac6307c41dda12c1f59331fb3b
Alexander Daley <alexander.daley@mymail.champlain.edu>
2025-09-28 18:19:03 -0400

    Finished Web Scraping assignment

--- week5/main.ps1
+++ week5/main.ps1
@@ -3,4 +3,21 @@
 clear
 
 $table = gatherClasses
-$table
+#$table
+#$table | select "Class Code", Instructor, Location, Days, "Time Start", "Time End" | where { $_.Instructor -ilike "Furkan Paligu" }
+#$table | Where-Object  {($_.Location -ilike "JOYC 310") -and ($_.days -ilike "Monday")} | `
+#         Sort-Object -Property "Time Start" | `
+#         select "Time Start", "Time End", "Class Code"
+
+$ITSInstructors = $table | Where-Object { ($_."Class Code" -ilike "SYS*") -or 
+                                              ($_."Class Code" -ilike "NET*") -or 
+                                              ($_."Class Code" -ilike "SEC*") -or 
+                                              ($_."Class Code" -ilike "FOR*") -or 
+                                              ($_."Class Code" -ilike "CSI*") -or 
+                                              ($_."Class Code" -ilike "DAT*") 
+                                              } | select "Instructor" `
+                                              | Sort-Object "Instructor" -Unique
+#$ITSInstructors
+
+$table | Where-Object { $_.Instructor -in $ITSInstructors.Instructor } `
+                | Group-Object "Instructor"  | Select-Object Count, Name | Sort-Object Count -Descending
--- week5/scrapingChamplainClasses.ps1
+++ week5/scrapingChamplainClasses.ps1
@@ -14,10 +14,32 @@ for($i = 1; $i -lt $trs.length; $i ++)
     $fullTable += [pscustomobject]@{"Class Code" = $tds[0].outerText;
                                     "Title" = $tds[1].outerText;
                                     "Days" = $tds[4].outerText;
-                                    "Time Start" = $Times[0].outerText;
-                                    "Time End" = $Times[1].outerText;
+                                    "Time Start" = $Times[0];
+                                    "Time End" = $Times[1];
                                     "Instructor" = $tds[6].outerText;
                                     "Location" = $tds[9].outerText; }
 }
+$fullTable = daysTranslator $fullTable
 return $fullTable
+}
+
+function daysTranslator($fullTable){
+    for($i = 0; $i -lt $fullTable.length; $i++)
+    {
+        $days = @()
+        if($fullTable[$i].Days -ilike "*M*"){$days += "Monday"}
+
+        if($fullTable[$i].Days -ilike "*[T]*"){$days += "Tuesday"}
+        ElseIf($fullTable[$i].Days -ilike "T"){$days += "Tuesday"}
+
+        if($fullTable[$i].Days -ilike "*W*") {$days += "Wednesday"}
+
+        if($fullTable[$i].Days -ilike "*TH*") {$days += "Thursday"}
+
+        if($fullTable[$i].Days -ilike "*F*") {$days += "Friday"}
+
+        $fullTable[$i].Days = $days
+    }
+
+    return $fullTable
 }
