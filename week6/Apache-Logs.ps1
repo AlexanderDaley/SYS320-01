2a713e30ee6cb09b387d9295b30645a87a22ebb7
Alexander Daley <alexander.daley@mymail.champlain.edu>
2025-10-04 19:43:41 -0400

    Finished turn to menu

---
+++ week6/Apache-Logs.ps1
@@ -0,0 +1,42 @@
+﻿function getIPsVisited($pageName, $HTTPCode, $browserName) {
+    $notFounds = Get-Content C:\xampp\apache\logs\access.log | Select-String $HTTPCode.ToString()
+    $founds = $notFounds | Select-String $browserName.ToString()
+    $page = $founds | Select-String $pageName.ToString()
+
+    $regex = [regex] "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+
+    $ipsUnorganized = $regex.Matches($page)
+
+    $ips = @()
+    for($i=0; $i -lt $ipsUnorganized.Count; $i++)
+    { 
+       $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value;}
+    }
+
+    $ipCounts = $ips | Where-Object { $_.IP -ilike "10.*" }
+
+    $counts = $ipCounts | Group-Object -Property IP
+    $counts | Select-Object Count, Name
+}
+
+function ApacheLogs1(){
+    $logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+    $i = [System.Math]::Max($logsNotFormatted.Length - 11, 0)
+
+    for($i; $i -lt $logsNotFormatted.Length; $i++){
+        $words = $logsNotFormatted[$i].Split(" ");
+
+        $tableRecords += [pscustomobject]@{ "IP" = $words[0];
+                                            "Time" = $words[3].Trim("[");
+                                           "Method" = $words[4].Trim('"');
+                                          "Page" = $words[5];
+                                          "Protocol" = $words[6];
+                                          "Response" = $words[7];
+                                          "Referrer" = $words[8];
+                                          "Client" = $words[11..($words.Count)];
+                                          }
+    }
+
+    return $tableRecords
+}
---
+++ week6/TurnToMenu.ps1
@@ -0,0 +1,73 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation: `n"
+$Prompt += "1 - Display last 10 apache logs `n"
+$Prompt += "2 - Display last 10 failed logins for all users `n"
+$Prompt += "3 - Display at risk users `n"
+$Prompt += "4 - Start champlain.edu web page `n"
+$Prompt += "5 - Quit `n"
+
+$operation = $true
+
+while($operation)
+{
+
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host
+
+    if($choice -eq 1)
+    {
+        $table = ApacheLogs1
+        $table
+    }
+    elseIf($choice -eq 2)
+    {
+        $table = getFailedLogins 100000 | Group-Object count, name
+        $i = [System.Math]::Max($table.Length - 11, 0)
+        $finalTable = @()
+        for($i; $i -lt $table.Length; $i++)
+        {
+            $finalTable += $table[$i]
+        }
+        $finalTable
+    }
+    elseIf($choice -eq 3)
+    {
+        $userLogins = getFailedLogins 100000 | Group-Object count, name
+        $riskUsers = @()
+        foreach($item in $userLogins)
+        {
+            if($item.count > 9)
+            {
+                $riskUsers += $item.name
+            }
+        }
+        $riskUsers
+    }
+    elseIf($choice -eq 4)
+    {
+        if(Get-Process -Name "TurnToMenu" -ErrorAction SilentlyContinue)
+        {
+            Write-Host "Exiting"
+            Exit
+        }else{
+            Start-Process chrome.exe -ArgumentList "--new-window https://www.champlain.edu"
+            Write-Host "Tab Opened"
+        }
+    }
+    elseIf($choice -eq 5)
+    {
+        $operation = $True
+        Write-Host "Exiting" | Out-String
+        Exit
+    }
+    else
+    {
+        Write-Host "Your input is not valid here" | Out-String
+    }
+}
